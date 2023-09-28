2855b4555893ff7ac2904ad3b1c4fd1bea2cc1e2
nodgez9 <nodgez@9thimpact.com>
2023-09-28 10:08:41 +0100

    Removed trailing zero from  session name

--- Assets/Dedicated Server/DedicatedServer.cs
+++ Assets/Dedicated Server/DedicatedServer.cs
@@ -5,6 +5,7 @@ using Fusion.Sockets;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -23,15 +24,11 @@ public class DedicatedServer : MonoBehaviour
 #if UNITY_SERVER
         Application.targetFrameRate = 30;
 
-        //for (int i = 0; i < 6; i++)
-        //{
-        var i = 0;
-        var fullSessionName = string.Format("{0} {1}", sessionName, i);
         var runner = Instantiate(runnerPrefab);
-        runner.name = fullSessionName;
+        runner.name = sessionName;
         var startArgs = new StartGameArgs()
         {
-            SessionName = fullSessionName,
+            SessionName = sessionName,
             GameMode = GameMode.Server,
             SceneManager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>(),
             Scene = 1,
@@ -39,16 +36,15 @@ public class DedicatedServer : MonoBehaviour
             PlayerCount = 2
         };
 
-        print(string.Format("Starting session {0} {1}.....", sessionName, i));
+        print(string.Format("Starting session {0}.....", sessionName));
         var result = await runner.StartGame(startArgs);
         if (result.Ok)
-            print(string.Format("Session {0} {1} started!", sessionName, i));
+            print(string.Format("Session {0} started!", sessionName));
         else
-            print(string.Format("Session {0} {1} NOT started!", sessionName, i));
+            print(string.Format("Session {0} NOT started!", sessionName));
 
         if (!result.Ok)
             Application.Quit();
-        //}
 
 #endif
         SceneManager.LoadScene(1);
